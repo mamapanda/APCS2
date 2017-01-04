a307561016bd4a51eae58eb35017a1ada42660c4
panda <daniel.phan36@gmail.com>
2017-01-04 10:26:56 -0800

    added selection sort

---
+++ Sort/src/main/java/SelectionSort.java
@@ -0,0 +1,72 @@
+/**
+ * Generates random numbers and uses the selection sort to sort them.
+ *
+ * @author Daniel Phan
+ * @version 1.4.16
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int count;
+        int[] nums;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.printf("Number of elements in array: ");
+        count = in.nextInt();
+        nums = genNumbers(count);
+        System.out.println(Arrays.toString(nums));
+        selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * Generates an array of random numbers.
+     * (Postcondition: an array of length count with random numbers is returned)
+     * @param count the number of elements in the array
+     * @return an array with random numbers
+     * (Precondition: count > 0)
+     */
+    public static int[] genNumbers(int count) {
+        int i, upperBound;
+        int[] nums;
+        Random random;
+
+        upperBound = 100;
+        nums = new int[count];
+        random = new Random();
+
+        for (i = 0; i < count; i++) {
+            nums[i] = random.nextInt(upperBound);
+        }
+        return nums;
+    }
+
+    /**
+     * Sorts the given array.
+     * (Postcondition: nums is changed and sorted)
+     * @param nums the array of numbers to sort
+     * (Precondition: nums contains numbers)
+     */
+    public static void selectionSort(int[] nums) {
+        int i, j, min;
+
+        for (i = 0; i < nums.length; i++) {
+            min = i;
+            for (j = i; j < nums.length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            if (nums[i] != nums[min]) {
+                nums[i] ^= nums[min];
+                nums[min] ^= nums[i];
+                nums[i] ^= nums[min];
+            }
+        }
+    }
+}
