b13c991534414cd5ac07443a5ae3f1a5fb208462
panda <daniel.phan36@gmail.com>
2017-01-04 14:09:23 -0800

    split sort junit into multiple methods

--- Sort/src/test/java/SelectionSortTest.java
+++ Sort/src/test/java/SelectionSortTest.java
@@ -5,28 +5,42 @@ import static org.junit.Assert.*;
  */
 public class SelectionSortTest {
     @org.junit.Test
-    public void selectionSort() throws Exception {
+    public void selectionSort1() throws Exception {
         int[] input, expected;
 
-        input = new int[]{ 100, 32, 0, -3, -444, 666, 3 };
-        expected = new int[]{ -444, -3, 0, 3, 32, 100, 666 };
-        SelectionSort.selectionSort(input);
-        assertArrayEquals(expected, input);
-
-        input = new int[]{ 666, 666, 666, 666, 666, 0 };
-        expected = new int[]{ 0, 666, 666, 666, 666, 666 };
-        SelectionSort.selectionSort(input);
-        assertArrayEquals(expected, input);
-
-        input = new int[]{ 0, 1, 2, 3, 1, 2, 3, 1, 2, 3, 0 };
-        expected = new int[]{ 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3 };
-        SelectionSort.selectionSort(input);
-        assertArrayEquals(expected, input);
-
-        input = new int[]{ 1, 2, 3, 4, 5 };
-        expected = new int[]{ 1, 2, 3, 4, 5 };
+        input = new int[]{100, 32, 0, -3, -444, 666, 3};
+        expected = new int[]{-444, -3, 0, 3, 32, 100, 666};
         SelectionSort.selectionSort(input);
         assertArrayEquals(expected, input);
     }
 
+    @org.junit.Test
+    public void selectionSort2() throws Exception {
+        int[] input, expected;
+
+        input = new int[]{666, 666, 666, 666, 666, 0};
+        expected = new int[]{0, 666, 666, 666, 666, 666};
+        SelectionSort.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @org.junit.Test
+    public void selectionSort3() throws Exception {
+        int[] input, expected;
+
+        input = new int[]{0, 1, 2, 3, 1, 2, 3, 1, 2, 3, 0};
+        expected = new int[]{0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3};
+        SelectionSort.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @org.junit.Test
+    public void selectionSort4() throws Exception {
+        int[] input, expected;
+
+        input = new int[]{1, 2, 3, 4, 5};
+        expected = new int[]{1, 2, 3, 4, 5};
+        SelectionSort.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
 }
