da2e840c01d6d97352da39eae39767bcda2fe279
panda <daniel.phan36@gmail.com>
2017-01-04 14:52:11 -0800

    better junit for selectionsort

--- Sort/src/test/java/SelectionSortTest.java
+++ Sort/src/test/java/SelectionSortTest.java
@@ -1,46 +1,45 @@
 import static org.junit.Assert.*;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * @author Daniel Phan
  */
+@RunWith(Parameterized.class)
 public class SelectionSortTest {
-    @org.junit.Test
-    public void selectionSort1() throws Exception {
-        int[] input, expected;
+    public SelectionSortTest(int[] input, int[] expected) {
+        input_ = input;
+        expected_ = expected;
+    }
 
-        input = new int[]{100, 32, 0, -3, -444, 666, 3};
-        expected = new int[]{-444, -3, 0, 3, 32, 100, 666};
-        SelectionSort.selectionSort(input);
-        assertArrayEquals(expected, input);
+    @Parameterized.Parameters
+    public static Collection<int[][]> cases() {
+        return Arrays.asList(new int[][][]{{
+                new int[]{100, 32, 0, -3, -444, 666, 3},
+                new int[]{-444, -3, 0, 3, 32, 100, 666}
+        }, {
+                new int[]{666, 666, 666, 666, 666, 0},
+                new int[]{0, 666, 666, 666, 666, 666}
+        }, {
+                new int[]{0, 1, 2, 3, 1, 2, 3, 1, 2, 3, 0},
+                new int[]{0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3}
+        }, {
+                new int[]{1, 2, 3, 4, 5},
+                new int[]{1, 2, 3, 4, 5}
+        }
+        });
     }
 
     @org.junit.Test
-    public void selectionSort2() throws Exception {
-        int[] input, expected;
-
-        input = new int[]{666, 666, 666, 666, 666, 0};
-        expected = new int[]{0, 666, 666, 666, 666, 666};
-        SelectionSort.selectionSort(input);
-        assertArrayEquals(expected, input);
+    public void selectionSort() throws Exception {
+        SelectionSort.selectionSort(input_);
+        assertEquals(Arrays.toString(expected_), Arrays.toString(input_));
     }
 
-    @org.junit.Test
-    public void selectionSort3() throws Exception {
-        int[] input, expected;
-
-        input = new int[]{0, 1, 2, 3, 1, 2, 3, 1, 2, 3, 0};
-        expected = new int[]{0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3};
-        SelectionSort.selectionSort(input);
-        assertArrayEquals(expected, input);
-    }
-
-    @org.junit.Test
-    public void selectionSort4() throws Exception {
-        int[] input, expected;
-
-        input = new int[]{1, 2, 3, 4, 5};
-        expected = new int[]{1, 2, 3, 4, 5};
-        SelectionSort.selectionSort(input);
-        assertArrayEquals(expected, input);
-    }
+    private int[] input_;
+    private int[] expected_;
 }
